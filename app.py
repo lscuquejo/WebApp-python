9e97a34d5805378677ecbf5ecde0da0e5a73d815
lscuquejo <leonardo.soares@technest.es>
2020-06-30 19:52:33 +0200

    all requests should be working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -20,12 +20,22 @@ stores = [
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>') # localhost/store/some_name
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
 
 # GET /store
 @app.route('/store')
@@ -35,11 +45,31 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'items': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    pass
+def get_items_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
+# # GET /store/<string:name>/item
+# @app.route('/store/<string:name>/item')
+# def get_item_in_store(name):
+#     for store in stores:
+#         for item in store['items']:
+#             if item == name:
+#                 return jsonify(store)
+#         return jsonify({'message': 'item not found'})
 app.run(port=5000)
